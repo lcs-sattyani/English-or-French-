d00d0b5db521173e6aec67e7d2f87f3814b63daa
Sawsan Attyani <sattyani@lcs.on.ca>
2019-11-26 10:45:56 -0500

    Initial Commit

---
+++ English or French?/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  English or French?
+//
+//  Created by Attyani, Sawsan on 2019-11-26.
+//  Copyright © 2019 Attyani, Sawsan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
